34dbb5a09654fc4b37b29eec0ab369542e1c5f71
‘Tyrone <trynarthurs@yahoo.com>
2017-08-10 06:21:46 -0400

    Initial Commit no modifications

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RawEpsilonVms.Startup))]
+namespace RawEpsilonVms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
